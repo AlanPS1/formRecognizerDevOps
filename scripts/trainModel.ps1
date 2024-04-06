49ad9e0961e3434b7dbad935dc9b4473d0175d28
Alan Wightman <alan@alanps1.onmicrosoft.com>
2024-04-06 21:42:07 +0100

    build : undo test

--- scripts/trainModel.ps1
+++ scripts/trainModel.ps1
@@ -30,10 +30,10 @@ try {
     
     $decryptedEndpoint = ConvertFrom-SecureString $functionEndpoint -AsPlainText
 
-    # $modelId = New-Guid
-    $stringDate = Get-Date -Format "dddd dd-MM-yy"
-    $modelId = "cv-$stringDate-v0.$($($(New-Guid) -split "-", 3)[1])"
-    
+    $modelId = New-Guid
+    # $stringDate = Get-Date -Format "dddd dd-MM-yy"
+    # $modelId = "cv-$stringDate-v0.$($($(New-Guid) -split "-", 3)[1])"
+
     $description = "Job Model for $modelId"
 
     $body= @{
